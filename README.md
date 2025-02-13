289627f3c99350a68aae8dcfb3187bb1b99b213c
andrewjpascual <andrewjpascual@yahoo.com>
2025-02-12 23:39:26 -0800

    Merge changes

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # PyScript
 My personal Python scripts and projects
 
-# 2/10/25
+## 2/10/25
 The changes on this branch includes the initial build of the security script within main.py
 
-# 2/11/25
+## 2/11/25
 Introduce checks on specific Window directories to identify potential vulnerabilities
+
+## 2/12/25
+Continue checks from yesterday
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 
 #Specify path
-path1 = r'C:\Users\andre\OneDrive\Desktop'
+path1 = r'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols'
 if os.path.exists(path1):
     print ("This directory " +path1+  " exists")
 else:
-    print ("This directory " +path1+ " does not exist! Skipping directory...")
+    print ("This directory " +path1+ " does not exist! Skipping directory....")
